46a96fce14c545f75bda6051b2594e9f613eec45
buoriky7 <buoriky7@gmail.com>
2019-06-06 09:15:06 +0000

    gem-finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_082211) do
+ActiveRecord::Schema.define(version: 2019_06_06_085441) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "word_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "name"
+    t.integer "age"
+    t.string "prefecture"
+    t.string "user_gender"
+    t.integer "user_flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "words", force: :cascade do |t|
