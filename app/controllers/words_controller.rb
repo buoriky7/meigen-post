9a92686b4031e9e76a2ebdc843c359914afedf67
buoriky7 <buoriky7@gmail.com>
2019-06-09 08:17:29 +0000

    add

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,17 @@
 class WordsController < ApplicationController
   def new
+    @word = Word.new
+  end
+
+  def create
+    @word = Word.new(word_params)
+    @word.user_id = current_user.id
+     if @word.save(word_params)
+        redirect_to '/words'
+        flash[:notice] = "successfully"
+     else
+        redirect_back(fallback_location: root_path)
+    end
   end
 
   def index
@@ -16,4 +28,16 @@ class WordsController < ApplicationController
 
   def edit
   end
+
+    private
+    def word_params
+     params.require(:word).permit(:speaker,
+                                  :genre,
+                                  :speaker_gender,
+                                  :sp_word,
+                                  :commentary,
+                                  :image,
+                                  :word_flag)
+    end
+
 end
