8f52ef2b57b5699cad2a577c4a84c1c7aba69696
buoriky7 <buoriky7@gmail.com>
2019-06-10 09:56:59 +0000

    add

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,10 +1,16 @@
 class Word < ApplicationRecord
 	belongs_to :user
+    has_many :favorites, dependent: :destroy
+    def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    end
+
 	attachment :image
-	validates :speaker, presence: true, length: { in: 2..30 }
+	validates :speaker, presence: true, length: { in: 1..30 }
 	validates :speaker_gender, presence: true
 	validates :sp_word, presence: true, length: { in: 3..100 }
 	validates :commentary, presence: true, length: { maximum: 200 }
 	validates :word_flag, presence: true
 	validates :genre, presence: true, length: { in: 1..10 }
+
 end
