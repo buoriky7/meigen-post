12175c476e14e613f01ec7c10c3219da8b10fbf7
buoriky7 <buoriky7@gmail.com>
2019-06-12 09:13:43 +0000

    add

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,6 @@
 class RootController < ApplicationController
   def top
   	 @word = Word.find(Word.favorite_ids.sample)
-  	 # @word = User.group("name")
   end
 
   def about
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
+    @user = User.all
+
   end
 
   def show
@@ -18,7 +22,17 @@ class UsersController < ApplicationController
 
   def new
   end
-
+  def destroy
+    @user = User.find(params[:id])
+    user = User.find(params[:id])
+      if current_user.user_flag == 1
+        user.destroy
+        redirect_to action: '/users'
+      else
+        user.destroy
+        redirect_to '/'
+      end
+  end
 
 
   private
